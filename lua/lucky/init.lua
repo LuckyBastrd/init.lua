71ac1071a3f927f8208e8621d1ac31bdca0501fd
LuckyBatrd <rafi.khairunizham@gmail.com>
2023-12-24 04:28:10 +0700

    small change

--- lua/lucky/init.lua
+++ lua/lucky/init.lua
@@ -1,12 +1,8 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
-
--- DO NOT INCLUDE THIS
-vim.opt.rtp:append("~/personal/streamer-tools")
--- DO NOT INCLUDE THIS
+require("lucky.set")
+require("lucky.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local LuckyGroup = augroup('Lucky', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -27,7 +23,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = LuckyGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
